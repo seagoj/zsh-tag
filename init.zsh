e1a8a228001e92fb8994ff0e23c6ec69f60ce9c8
Jeremy Seago <jeremy.seago@ispot.tv>
2024-10-13 14:56:12 -0500

    main: removes logging

--- init.zsh
+++ init.zsh
@@ -2,8 +2,6 @@ if [[ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/tag ]]; then
   mkdir "${XDG_DATA_HOME:-$HOME/.local/share}"/tag
 fi
 
-echo "$TAG_SEARCH_PROG"
-
 if (( $+commands[tag] )); then
   tag() { command tag "$@"; source ${TAG_ALIAS_FILE:-$HOME/tmp/tag_aliases} 2>/dev/null }
   alias "${TAG_SEARCH_PROG:-rg}"=tag
