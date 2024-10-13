99e2afd9a177beb68c56e31a8b51efc5f8f8db17
Jeremy Seago <jeremy.seago@ispot.tv>
2024-10-13 14:48:14 -0500

    main: Fallback to envvars

--- init.zsh
+++ init.zsh
@@ -2,11 +2,8 @@ if [[ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/tag ]]; then
   mkdir "${XDG_DATA_HOME:-$HOME/.local/share}"/tag
 fi
 
-local search_prog
-zstyle -s ':seagoj:tag:' search-prog search_prog
-
 if (( $+commands[tag] )); then
   tag() { command tag "$@"; source ${TAG_ALIAS_FILE:$HOME/tmp/tag_aliases} 2>/dev/null }
-  alias $search_prog=tag
+  alias "${TAG_SEARCH_PROG:-rg}"=tag
 fi
 
